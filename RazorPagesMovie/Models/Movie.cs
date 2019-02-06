b39dd8d595752135c81fef0b24530328e1d2daea
Andrew Tat <andrewtat@me.com>
2019-02-05 18:50:27 -0800

    Add ASP.NET Razor tutorial (through Scaffolding)

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        public Movie()
+        {
+        }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
