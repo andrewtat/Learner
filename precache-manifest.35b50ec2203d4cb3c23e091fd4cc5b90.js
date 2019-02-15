d0c82d0fe09e1874269d7b8d466cf6820e70c311
Andrew Tat <andrewtat@me.com>
2019-02-14 16:31:37 -0800

    Updates

---
+++ precache-manifest.35b50ec2203d4cb3c23e091fd4cc5b90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7220e9c41c2d515829ee",
+    "url": "/tania-rascia-react-tutorial/static/js/runtime~main.7220e9c4.js"
+  },
+  {
+    "revision": "8c7f655f344732af612a",
+    "url": "/tania-rascia-react-tutorial/static/js/main.8c7f655f.chunk.js"
+  },
+  {
+    "revision": "1079982f45043f536abc",
+    "url": "/tania-rascia-react-tutorial/static/js/2.1079982f.chunk.js"
+  },
+  {
+    "revision": "8c7f655f344732af612a",
+    "url": "/tania-rascia-react-tutorial/static/css/main.d3ef50b4.chunk.css"
+  },
+  {
+    "revision": "23a60515a8cb0628afa226d38a140ee3",
+    "url": "/tania-rascia-react-tutorial/index.html"
+  }
+];
